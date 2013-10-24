ec22ed0077bf08ba21fad0494fccc3cae2e13cff
Josh Holtz <josh@rokkincat.com>
2013-10-24 09:53:38 -0500

    Added comments to SweetToothListener

--- Library/SweetTooth/src/com/joshholtz/SweetToothManager.java
+++ Library/SweetTooth/src/com/joshholtz/SweetToothManager.java
@@ -20,8 +20,26 @@ public class SweetToothManager {
 	public final static int REQUEST_BLE_ENABLE = 3011989;
 
 	public interface SweetToothListener {
+		
+		/**
+		 * Gets called when scanning starts or stops.
+		 * Called from start() and stop()
+		 * @param scanning
+		 */
 		public void onScanningStateChange(boolean scanning);
+		
+		/**
+		 * Gets called when startOnInterval() starts and stops scanning.
+		 * @param scanning
+		 */
 		public void onScanningIntervalStateChange(boolean scanning);
+		
+		/**
+		 * Gets called when a device is discovered.
+		 * @param device
+		 * @param rssi
+		 * @param scanRecord
+		 */
 		public void onDiscoveredDevice(BluetoothDevice device, int rssi, byte[] scanRecord);
 	}
 	
